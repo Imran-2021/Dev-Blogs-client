95b8eda42d8fe08c57c8805785b0a1291cd79992
Imran-2021 <mdimranulhaque.8dec20@gmail.com>
2021-10-18 11:40:28 +0600

    start coding for home pg

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,9 +2,12 @@ import React from 'react';
 import "./Home.css"
 const Home = () => {
     return (
-        <div className="home">
+        <>
+        <div className="home" style={{backgroundImage: "linear-gradient(rgb(219 220 224 / 0%), rgb(255 255 255)), url(Images/devshare.jfif)"}}>
             <p>this is home !</p>
         </div>
+        <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia consequatur quibusdam soluta saepe ne?</p>
+        </>
     );
 };
 
